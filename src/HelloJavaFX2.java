fff59bba215761cc2ebc469e872be4be733c87ef
ursin <ursin@DESKTOP-FF37QEI.home>
2020-11-10 19:02:29 +0100

    Get started

---
+++ src/HelloJavaFX2.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+	import javafx.scene.Scene;
+	import javafx.scene.control.Label;
+	import javafx.scene.layout.BorderPane;
+	import javafx.stage.Stage;
+
+
+
+
+	
+	
+	public class HelloJavaFX2 extends Application {
+	public static void main(String[] args) {
+	launch();
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	Label lblHello = new Label("Hello, JavaFX!");
+	BorderPane root = new BorderPane();
+	root.setCenter(lblHello);
+	Scene scene = new Scene(root);
+	 primaryStage.setTitle("Hello, JavaFX!");
+	 primaryStage.setScene(scene);
+	 primaryStage.show();
+	}
+}
